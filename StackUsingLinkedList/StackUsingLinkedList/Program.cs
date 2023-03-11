aca47ba1ceb7d56ccad09fc989a31c085f89903a
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-11 00:38:00 -0500

    [Ruchita] Add: Create stack using linked list

---
+++ StackUsingLinkedList/StackUsingLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StackUsingLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1 Ability to create a Stack of 56->30->70
+            //LIFO
+            //56 will be on the top of the stack
+            LinkListStack linkListStack = new LinkListStack ();
+            linkListStack.Push(70);
+            linkListStack.Push(30);
+            linkListStack.Push(56);
+            linkListStack.Display();
+        }
+    }
+    }
